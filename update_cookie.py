553424ce9dec4f24061524a3a2415c58fac1261d
Sagar878796 <sagarsutradhar046@gmail.com>
2026-04-05 09:53:47 +0530

    Update update_cookie.py

--- update_cookie.py
+++ update_cookie.py
@@ -1,4 +1,30 @@
 import requests
+import json
+
+URL = "https://raw.githubusercontent.com/Prtstream820894/prtstreams/refs/heads/main/ptt.m3u"
+
+response = requests.get(URL)
+lines = response.text.splitlines()
+
+channels = []
+
+for i in range(len(lines)):
+    if lines[i].startswith("#EXTINF"):
+        name = lines[i].split(",")[-1]
+        url = lines[i+1]
+
+        channels.append({
+            "name": name,
+            "logo": "",
+            "group": "Live",
+            "url": url
+        })
+
+# Save JSON
+with open("data.json", "w", encoding="utf-8") as f:
+    json.dump({"channels": channels}, f, indent=2)
+
+print("✅ JSON Created")import requests
 from datetime import datetime
 
 URL = "https://raw.githubusercontent.com/Prtstream820894/prtstreams/refs/heads/main/ptt.m3u"
