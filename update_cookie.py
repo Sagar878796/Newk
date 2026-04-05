25439bf21154c9e21ae62ec7698134644fdd219d
Sagar878796 <sagarsutradhar046@gmail.com>
2026-04-05 10:00:18 +0530

    Update update_cookie.py

--- update_cookie.py
+++ update_cookie.py
@@ -20,23 +20,7 @@ for i in range(len(lines)):
             "url": url
         })
 
-# Save JSON
 with open("data.json", "w", encoding="utf-8") as f:
     json.dump({"channels": channels}, f, indent=2)
 
-print("✅ JSON Created")import requests
-from datetime import datetime
-
-URL = "https://raw.githubusercontent.com/Prtstream820894/prtstreams/refs/heads/main/ptt.m3u"
-
-try:
-    r = requests.get(URL)
-    data = r.text
-
-    with open("min.m3u", "w", encoding="utf-8") as f:
-        f.write(data)
-
-    print("✅ Updated:", datetime.now())
-
-except Exception as e:
-    print("Working ✅")
+print("✅ JSON Created")
